5a402e6b50b39261bbb84ff3726cf95862e68de9
Michael Clark <michaeljclark@mac.com>
2024-02-25 14:32:54 +1300

    implement aligned_alloc to split blocks to the alignment

--- guest/include/stdlib.h
+++ guest/include/stdlib.h
@@ -9,6 +9,7 @@ extern "C" {
 __attribute__((noreturn)) void abort(void);
 __attribute__((noreturn)) void exit(int status);
 void* malloc(size_t size);
+void *aligned_alloc( size_t alignment, size_t size );
 void free(void* ptr);
 void _malloc_addblock(void* addr, size_t size);
 void qsort (void *, size_t, size_t, int (*)(const void *, const void *));
--- guest/libc/malloc.c
+++ guest/libc/malloc.c
@@ -11,14 +11,14 @@ typedef struct alloc_node
 {
 	struct list_head node;
 	size_t size;
-	char* block;
+	char block[0];
 } alloc_node_t;
 
 /* allocation metadata size */
 #define ALLOC_HEADER_SZ __builtin_offsetof(alloc_node_t, block)
 
-/* minimum allocation size of 32 bytes */
-#define MIN_ALLOC_SZ ALLOC_HEADER_SZ + 32
+/* minimum allocation size of four pointers (32 bytes) */
+#define MIN_ALLOC_SZ (ALLOC_HEADER_SZ + sizeof(void*))
 
 /* free list */
 static LIST_HEAD(free_list);
@@ -43,44 +43,69 @@ static void coalesce_free_list(void)
 	}
 }
 
-void* malloc(size_t size)
+static int ispow2(size_t v)
 {
-	void* ptr = NULL;
-	alloc_node_t* blk = NULL;
+	return !(v & (v-1));
+}
 
-	if (size > 0)
+void *aligned_alloc(size_t align, size_t size)
+{
+	alloc_node_t* blk = NULL, *alloc_blk = NULL, *new_blk;
+	uintptr_t slack = 0;
+
+	if (size == 0 || !align || !ispow2(align)) return NULL;
+
+	/* Align the pointer */
+	size = ALIGN_SIZE(size, sizeof(void*));
+
+	/* try to find a big enough block */
+	list_for_each_entry(blk, &free_list, node)
 	{
-		/* Align the pointer */
-		size = ALIGN_SIZE(size, sizeof(void*));
+		/* calculate slack to align an unaligned block */
+		uintptr_t start = (uintptr_t)&blk->block;
+		uintptr_t end = ALIGN_SIZE(start, align);
+		while (end - start != 0 &&
+			   end - start < ALLOC_HEADER_SZ) end += align;
+		slack = end - start;
 
-		/* try to find a big enough block */
-		list_for_each_entry(blk, &free_list, node)
+		/* break if the current block is big enough */
+		if (blk->size >= size + slack)
 		{
-			if (blk->size >= size)
-			{
-				ptr = &blk->block;
-				break;
-			}
+			alloc_blk = blk;
+			break;
 		}
-
-		if (ptr)
-		{
-			/* split block if possible */
-			if ((blk->size - size) >= MIN_ALLOC_SZ)
-			{
-				alloc_node_t* new_blk;
-				new_blk = (alloc_node_t*)((uintptr_t)(&blk->block) + size);
-				new_blk->size = blk->size - size - ALLOC_HEADER_SZ;
-				blk->size = size;
-				list_add(&new_blk->node, &blk->node);
-			}
-
-			list_del(&blk->node);
-		}
-
 	}
 
-	return ptr;
+	if (!alloc_blk) return NULL;
+
+	/* split block for alignment if necessary */
+	if (slack > 0) {
+		uintptr_t start = (uintptr_t)&alloc_blk->block;
+		new_blk = (alloc_node_t*)(start + slack - ALLOC_HEADER_SZ);
+		new_blk->size = alloc_blk->size - slack;
+		alloc_blk->size = slack - ALLOC_HEADER_SZ;
+		list_add(&new_blk->node, &alloc_blk->node);
+		alloc_blk = new_blk;
+	}
+
+	/* split remainder of block if possible */
+	if ((alloc_blk->size - size) >= MIN_ALLOC_SZ)
+	{
+		uintptr_t start = (uintptr_t)&alloc_blk->block;
+		new_blk = (alloc_node_t*)(start + size);
+		new_blk->size = alloc_blk->size - size - ALLOC_HEADER_SZ;
+		alloc_blk->size = size;
+		list_add(&new_blk->node, &alloc_blk->node);
+	}
+
+	list_del(&alloc_blk->node);
+
+	return &alloc_blk->block;
+}
+
+void* malloc(size_t size)
+{
+	return aligned_alloc(sizeof(void*), size);
 }
 
 void free(void* ptr)
